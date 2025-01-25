94b90732c550111c53309a9690b9f5380757628c
Ekaterina Romanova <Dekaterina83@mail.ru>
2025-01-25 12:07:43 +0300

    tests changed

--- src/test/java/CardDeliverySelenidTest.java
+++ src/test/java/CardDeliverySelenidTest.java
@@ -1,46 +1,36 @@
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.*;
-import static java.time.Duration.ofSeconds;
-
-
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class CardDeliverySelenidTest {
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
 
-    String planningDate = generateDate(5);
-    SelenideElement notification = $x("//div[@data-test-id='notification']");
-
-
-    @BeforeEach
-    public void setUp() {
-        open("http://localhost:9999/");
+    private String setDate(long daysToAdd, String pattern) {
+        return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
-    public void ShouldCardDelivery() {
-        $x(".//span[@data-test-id='city']//input").setValue("Воронеж");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(planningDate);
-        $x(".//span[@data-test-id='name']//input").setValue("Николаев Николай");
-        $x(".//span[@data-test-id='phone']//input").setValue("+71234567891");
-        $x(".//label[@data-test-id='agreement']").click();
-        $x(".//span[contains(text(),'Забронировать')]").click();
-        notification.should(visible, ofSeconds(15));
-        notification.$x(".//div[@class='notification__title']").should(text("Успешно!"));
-        notification.$x(".//div[@class='notification__content']").should(text("Встреча успешно забронирована на " + planningDate));
-        notification.$x(".//button").click();
-        notification.should(hidden);
+    void shouldCheckPositiveCase() {
+
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Воронеж");
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").sendKeys(BACK_SPACE);
+        String newDate = setDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(newDate);
+        $("[data-test-id=name] input").setValue("Николаев Николай");
+        $("[data-test-id=phone] input").setValue("+71234567891");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + newDate));
 
     }
 
